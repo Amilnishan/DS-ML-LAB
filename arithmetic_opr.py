f269d4d36b17a48b4ecd3742f827e6e62003e88c
Amilnishan <amilnishan7@gmail.com>
2025-07-08 10:39:58 +0530

    Add files via upload

---
+++ arithmetic_opr.py
@@ -0,0 +1,11 @@
+print("Arithmetic Operations")
+
+num1=int(input("enter a number:"))
+num2=int(input("enter another number:"))
+
+print("addition=",num1+num2)
+print("subtraction=",num1-num2)
+print("multiplication=",num1*num2)
+print("division=",num1/num2)
+print("modulus=",num1%num2)
+
